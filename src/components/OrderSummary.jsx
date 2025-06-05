118d81cac9258507343cca4cededd45e9c34299f
kaveesha824 <kaveesha365@gmail.com>
2025-06-06 00:14:47 +0530

    Add Take Away

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -33,6 +33,7 @@ const OrderSummary = ({ selectedTable, onClearTable }) => {
   } = useCart();
   const {
     activeTakeAwayOrder,
+    takeAwayOrderNumber, 
     cartDetails,
     updateCartDetails,
     clearActiveOrder,
